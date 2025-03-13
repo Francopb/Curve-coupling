3b316c7e262ab10e65ed57dab854d34f312f8967
Francopb <francopb.20@gmail.com>
2025-03-13 10:03:47 +0100

    Improved graph algorithm and test

--- src/curveCoupling/compliantElements/graphAnalysis.py
+++ src/curveCoupling/compliantElements/graphAnalysis.py
@@ -12,7 +12,6 @@ def generate_network_equations(edges: List[Tuple[str, str]],
     
     Parameters:
     - edges (List[Tuple[str, str]]): List of tuples representing edges (e.g., [('A', 'B'), ('B', 'C'), ('C', 'A')]).
-              For multi-edges, include a unique identifier for each edge.
     - return_in_matrices (bool): Whether to return the results in matrix form.
     
     Returns:
@@ -27,96 +26,87 @@ def generate_network_equations(edges: List[Tuple[str, str]],
     - Joint output matrices.
     """
     
+    labeled_edges = [(e[0], e[1], {'idx' : i, 'origin':e[0], 'destination': e[1], 'data':e[2:]}) for i,e in enumerate(edges)]
+
+    MultiG = nx.MultiGraph()
+    MultiG.add_edges_from(labeled_edges)
+
     # Step 1: Kirchhoff's Current Law (for Forces)
+    force_out = np.zeros(len(edges))
     force_constr = []
-    nodes = set(n for e in edges for n in e)
-
-    for node in nodes:
-        if node == 'Start':
-            force_out = np.zeros(len(edges))
-            for i, edge in enumerate(edges):
-                if edge[0] == node:  # Force leaving the node
-                    force_out[i] = 1.0
-                elif edge[1] == node:  # Force entering the node
-                    force_out[i] = -1.0
-        elif node == 'End':
+    for node in MultiG.nodes:
+        if node == 'End':
             continue
-        else:
-            # Sum of forces entering and leaving the node
-            force_sum = np.zeros(len(edges))
-            for i, edge in enumerate(edges):
-                if edge[0] == node:  # Force leaving the node
-                    force_sum[i] = 1.0
-                elif edge[1] == node:  # Force entering the node
-                    force_sum[i] = -1.0
-            if np.sum(force_sum**2 > 0):
-                force_constr.append(force_sum)  # Sum of forces = 0
-    
-    # Step 2: Kirchhoff's Voltage Law (for Displacements)
-    # Represent graph as a MultiGraph to handle multi-edges
-    G = nx.Graph()
-    G.add_edges_from(edges)
-    cycles = nx.cycle_basis(G)  # Find independent cycles in the graph
+        
+        connected_edges = MultiG.edges(node, data=True)
+        force_sum = np.zeros(len(edges))
+        for edge in connected_edges:
+            idx = edge[2]['idx']
+            if edge[2]['origin'] == node:  # Force leaving the node
+                force_sum[idx] = 1.0
+            elif edge[2]['destination'] == node:  # Force entering the node
+                force_sum[idx] = -1.0
 
+        if np.sum(force_sum**2 > 0):
+            if node == 'Start':
+                force_out = force_sum
+            else:
+                force_constr.append(force_sum)  # Sum of forces = 0
+
+    G = nx.Graph(MultiG)
+    cycles = nx.cycle_basis(G)  # Find independent cycles in the graph (not defined for MultiGrapg)
+    # Step 2: Kirchhoff's Voltage Law (for Displacements)
     disp_constr = []
     for cycle in cycles:
         disp_sum = np.zeros(len(edges))
-        for i in range(len(cycle)):
-            start = cycle[i]
-            end = cycle[(i + 1) % len(cycle)]  # Next node in the cycle
+        for k in range(len(cycle)):
+            u = cycle[k]
+            v = cycle[(k + 1) % len(cycle)]  # Next node in the cycle
             
-            # Identify the specific edge in the graph
-            edge = None
-            for i, e in enumerate(edges):
-                if (e[0] == start and e[1] == end) or (e[0] == end and e[1] == start):
-                    edge = e
-                    break
-            
-            # Add the voltage drop across the edge to the cycle sum
-            if edge:
-                if edge[0] == start:  # Voltage follows the traversal direction
-                    disp_sum[i] = 1.0
-                else:  # Voltage opposes the traversal direction
-                    disp_sum[i] = -1.0
-            else:
-                raise Exception("Sorry, no edge found.")
+            edge_data = G[u][v]
+
+            idx = edge_data['idx']
+            if edge_data['origin'] == u:  # Force leaving the node
+                disp_sum[idx] = 1.0
+            elif edge_data['origin'] == v:  # Force entering the node
+                disp_sum[idx] = -1.0
         disp_constr.append(disp_sum)  # Sum of displacements = 0
 
-    # Step 2: Check multiedges
-    for i1, e1 in enumerate(edges[:-1]):
-        for i_aux, e2 in enumerate(edges[i1 + 1:]):
-            i2 = i_aux + i1 + 1
-            if e1 == e2:
+    # Check multiedges
+    for edge in G.edges:
+        edge_data = MultiG.get_edge_data(edge[0],edge[1]).values()
+        e0 = None
+        for k, e in enumerate(edge_data):
+            if k == 0:
+                e0 = e
+                continue
+            
+            i1 = e0['idx']
+            i2 = e['idx']
+            if e0['origin'] == e['origin']:
                 disp_sum = np.zeros(len(edges))
                 disp_sum[i1] = 1.0
                 disp_sum[i2] = -1.0
                 disp_constr.append(disp_sum)
-            elif e1 == e2[::-1]:
+            elif e0['origin'] == e['destination']:
                 disp_sum = np.zeros(len(edges))
                 disp_sum[i1] = 1.0
                 disp_sum[i2] = 1.0
                 disp_constr.append(disp_sum)
 
-    path = nx.shortest_path(G, source='Start', target='End')
+    # Compute total displacement
+    start_end_path = nx.shortest_path(G, source='Start', target='End')
     disp_out = np.zeros(len(edges))
-    for i in range(len(path) - 1):
-        start = path[i]
-        end = path[i + 1]
+    for k in range(len(start_end_path) - 1):
+        u = start_end_path[k]
+        v = start_end_path[k + 1]
             
-        # Identify the specific edge in the graph
-        edge = None
-        for i, e in enumerate(edges):
-            if (e[0] == start and e[1] == end) or (e[0] == end and e[1] == start):
-                edge = e
-                break
-
-        if edge:
-            if edge[0] == start:  # Voltage follows the traversal direction
-                disp_out[i] = 1.0
-            else:  # Voltage opposes the traversal direction
-                disp_out[i] = -1.0
-        else:
-            raise Exception("Sorry, no edge found.")
+        edge_data = G[u][v]
+        idx = edge_data['idx']
+        if edge_data['origin'] == u:  # Force leaving the node
+            disp_out[idx] = 1.0
+        elif edge_data['origin'] == v:  # Force entering the node
+            disp_out[idx] = -1.0
         
     disp_constr = np.array(disp_constr)
     force_constr = np.array(force_constr)
@@ -125,7 +115,6 @@ def generate_network_equations(edges: List[Tuple[str, str]],
         return _split2joint_constr([disp_constr, force_constr]), _split2joint_out([disp_out, force_out])
     else:
         return disp_constr, force_constr, disp_out, force_out
-
 
 # Author: Franco N. Pinan Basualdo
 # Project: Curve Coupling
--- tests/Test_graphAnalysis.py
+++ tests/Test_graphAnalysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 from curveCoupling.compliantElements import generate_network_equations
 
 def run():
@@ -27,8 +28,52 @@ def run():
     print()
     print("OutputMatrices:\n", OutputMatrices)
 
+def test_series(n: int = 2):
+    num_intermediate_nodes = n - 1
+    edges = []
+    edges.append(("Start",str(0)))
+    for i in range(num_intermediate_nodes-1):
+        edges.append((str(i),str(i+1)))
+    edges.append((str(num_intermediate_nodes-1), "End"))
+    disp_constr, force_constr, disp_out, force_out = generate_network_equations(edges)
+
+    assert disp_constr.size == 0, "There should be no displacement constraints"
+    frc_vec = np.ones(n)
+    err = np.dot(force_constr, frc_vec)
+    assert np.sum(err**2) == 0, "This should be a possible force variation"
+
+    for i in range(n):
+        frc_vec = np.zeros(n)
+        frc_vec[i] = 1.0
+        err = np.dot(force_constr, frc_vec)
+        assert np.sum(err**2) >= 1, "This should not be a possible force variation"
+
+    assert np.all(disp_out==1.0), "The out disp. should be the sum of all"
+    assert np.sum(force_out == 1) == 1 and np.all(force_out[np.where(force_out != 1)] == 0), "A single out force should be one and the rest zero"
+
+
+def test_parallel(n: int = 2):
+    edges = [("Start","End")]*n
+    disp_constr, force_constr, disp_out, force_out = generate_network_equations(edges)
+
+    assert force_constr.size == 0, "There should be no force constraints"
+    disp_vec = np.ones(n)
+    err = np.dot(disp_constr, disp_vec)
+    assert np.sum(err**2) == 0, "This should be a possible displacement variation"
+
+    for i in range(n):
+        disp_vec = np.zeros(n)
+        disp_vec[i] = 1.0
+        err = np.dot(disp_constr, disp_vec)
+        assert np.sum(err**2) >= 1, "This should not be a possible displacement variation"
+
+    assert np.all(force_out==1.0), "The out force. should be the sum of all"
+    assert np.sum(disp_out == 1) == 1 and np.all(disp_out[np.where(disp_out != 1)] == 0), "A single out disp. should be one and the rest zero"
+    
 if __name__ == "__main__":
-    run()
+    # run()
+    test_series(5)
+    test_parallel(5)
 
 # Author: Franco N. Pinan Basualdo
 # Project: Curve Coupling
