40d57b39e3154fb42da959520a592a9b4fec2599
Francopb <francopb.20@gmail.com>
2025-06-27 11:40:22 +0200

    Add plotResults_unstable_eigen function for visualizing unstable eigenvalues

--- src/curveCoupling/utils/defaultPlots.py
+++ src/curveCoupling/utils/defaultPlots.py
@@ -32,8 +32,9 @@ def plotResults(fig: Figure,
         axs[i].plot(d[:, 0], d[:, 1])
         axs[i].set_title("Curve "+str(i))
 
-        range_d = np.round(
-            np.stack((np.min(d, axis=0), np.max(d, axis=0)), axis=1), decimals=1)
+        min_d = np.min(d, axis=0)
+        max_d = np.max(d, axis=0)
+        range_d = np.column_stack((np.floor(10*min_d)/10, np.ceil(10*max_d)/10))
 
         axs[i].set_xlim(range_d[0])
         axs[i].set_ylim(range_d[1])
@@ -87,7 +88,6 @@ def plotResults(fig: Figure,
     axs[-2].xaxis.set_major_locator(ticker.LinearLocator(3))
     axs[-2].yaxis.set_major_locator(ticker.LinearLocator(3))
     return axs
-
 
 def plotResults_stability(fig: Figure,
                           data: List[np.ndarray],
@@ -166,6 +166,92 @@ def plotResults_stability(fig: Figure,
     axs[-2].set_ylabel(r"$F_\mathrm{out}$")
     return axs
 
+def plotResults_unstable_eigen(fig: Figure,
+                          data: List[np.ndarray],
+                          data_unstable_eigen: List[np.ndarray],
+                          out_lst: List[np.ndarray],
+                          res_lst: List[np.ndarray],
+                          out_unstable_eigen_lst: List[np.ndarray]):
+
+    numCurves = len(data)
+    fig.clear()
+    plot_step = 2
+    gs = gridspec.GridSpec(2, plot_step * numCurves)
+    axs = []
+
+    aux_eigen_lst = [np.ravel(e) for lst in [data_unstable_eigen, out_unstable_eigen_lst] for e in lst]
+    if len(aux_eigen_lst) == 0:
+        max_eigen = None
+    else:
+        all_eigen = np.concatenate(aux_eigen_lst)
+        max_eigen = np.max(all_eigen)
+
+    print("Max eigen:", max_eigen)
+
+    for i in range(0, numCurves):
+        axs.append(fig.add_subplot(gs[0, plot_step * i:plot_step * (i + 1)]))
+    axs.append(fig.add_subplot(gs[1, numCurves:]))
+    if numCurves == 2:
+        axs.append(fig.add_subplot(gs[1, :numCurves]))
+    elif numCurves == 3:
+        axs.append(fig.add_subplot(gs[1, :numCurves], projection='3d'))
+    fig.tight_layout(pad=2, h_pad=3, w_pad=2)
+
+    for i, (d, e) in enumerate(zip(data, data_unstable_eigen)):
+        plot_unstable_eigen(axs[i], e, d[:, 0], d[:, 1], max_eigen=max_eigen)
+        axs[i].set_title("Curve "+str(i))
+
+        range_d = np.round(
+            np.stack((np.min(d, axis=0), np.max(d, axis=0)), axis=1), decimals=1)
+
+        axs[i].set_xlim(range_d[0])
+        axs[i].set_ylim(range_d[1])
+        axs[i].xaxis.set_major_locator(ticker.LinearLocator(3))
+        axs[i].yaxis.set_major_locator(ticker.LinearLocator(3))
+        axs[i].set_xlabel(r"$x_"+str(i)+r"$")
+        axs[i].set_ylabel(r"$F_"+str(i)+r"$")
+
+    min_res = np.min([np.min(res, axis=0) for res in res_lst], axis=0)
+    max_res = np.max([np.max(res, axis=0) for res in res_lst], axis=0)
+    range_res = np.round(np.stack((min_res, max_res), axis=1), decimals=1)
+
+    if numCurves == 2:
+        for i, (res, e) in enumerate(zip(res_lst, out_unstable_eigen_lst)):
+            plot_unstable_eigen(axs[-1], e, res[:, 0], res[:, 1], is_closed=i>0, max_eigen=max_eigen)
+    elif numCurves == 3:
+        for i, (res, e) in enumerate(zip(res_lst, out_unstable_eigen_lst)):
+            plot_unstable_eigen(axs[-1], e, res[:, 0], res[:, 1], res[:, 2], is_closed=i>0, max_eigen=max_eigen)
+
+    if numCurves == 3:
+        axs[-1].set_zlim(range_res[2])
+        axs[-1].zaxis.set_major_locator(ticker.LinearLocator(3))
+        axs[-1].set_zlabel(r"$t_2$")
+
+    axs[-1].set_title("Parametric space")
+    axs[-1].set_xlim(range_res[0])
+    axs[-1].set_ylim(range_res[1])
+    axs[-1].xaxis.set_major_locator(ticker.LinearLocator(3))
+    axs[-1].yaxis.set_major_locator(ticker.LinearLocator(3))
+    axs[-1].set_xlabel(r"$t_0$")
+    axs[-1].set_ylabel(r"$t_1$")
+    axs[-1].set_aspect('equal')
+
+    for i, (out, e) in enumerate(zip(out_lst, out_unstable_eigen_lst)):
+        plot_unstable_eigen(axs[-2], e, out[:, 0], out[:, 1], is_closed=i>0, max_eigen=max_eigen)
+
+    min_out = np.min([np.min(out, axis=0) for out in out_lst], axis=0)
+    max_out = np.max([np.max(out, axis=0) for out in out_lst], axis=0)
+    range_out = np.round(np.stack((min_out, max_out), axis=1), decimals=1)
+
+    axs[-2].set_title("Result curve")
+    axs[-2].set_xlim(range_out[0])
+    axs[-2].set_ylim(range_out[1])
+    axs[-2].xaxis.set_major_locator(ticker.LinearLocator(3))
+    axs[-2].yaxis.set_major_locator(ticker.LinearLocator(3))
+    axs[-2].set_xlabel(r"$x_\mathrm{out}$")
+    axs[-2].set_ylabel(r"$F_\mathrm{out}$")
+    return axs
+
 
 def plot_stability(ax, stability, x, y, z=None, is_closed=False, **lc_kwargs):
     custom_cmap = mcolors.LinearSegmentedColormap.from_list(
@@ -176,6 +262,26 @@ def plot_stability(ax, stability, x, y, z=None, is_closed=False, **lc_kwargs):
         colored_line_merged(ax, stability, x, y, is_closed=is_closed, norm=norm, cmap=custom_cmap, **lc_kwargs)
     else:
         colored_line_merged(ax, stability, x, y, z, is_closed=is_closed, norm=norm, cmap=custom_cmap, **lc_kwargs)
+
+def plot_unstable_eigen(ax, unstable_eigen, x, y, z=None, is_closed=False, max_eigen=None, **lc_kwargs):
+    # You can adjust vmin and vmax as needed
+    if max_eigen is None:
+        max_eigen = np.max(unstable_eigen)
+
+    values = [0, 1, 2, max_eigen]
+    colors = ["tab:green", "tab:olive", "tab:orange", "tab:red"]
+
+    # Create a custom colormap and normalization
+    norm = mcolors.Normalize(vmin=0, vmax=max_eigen)
+    positions = [(v - values[0]) / (values[-1] - values[0]) for v in values]
+
+    # Now use the positions in the colormap
+    custom_cmap = mcolors.LinearSegmentedColormap.from_list("custom_cmap", list(zip(positions, colors)))
+    
+    if z is None == 2:
+        colored_line_merged(ax, unstable_eigen, x, y, is_closed=is_closed, norm=norm, cmap=custom_cmap, **lc_kwargs)
+    else:
+        colored_line_merged(ax, unstable_eigen, x, y, z, is_closed=is_closed, norm=norm, cmap=custom_cmap, **lc_kwargs)
 
 # Author: Franco N. Pinan Basualdo
 # Project: Curve Coupling
